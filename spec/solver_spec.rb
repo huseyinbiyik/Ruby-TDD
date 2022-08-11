1f1626f8f79a00d407eeb29ca1f20eafa214ce39
Arturgouveia1970 <artgouveiag@gmail.com>
2022-08-11 14:15:30 +0100

    created reverse spec

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -2,19 +2,29 @@ require_relative '../solver'
 
 describe 'solver class' do
   solve = Solver.new
-  it 'factorial returns factorial of the N' do
-    result = solve.factorial(3)
-    expected_result = 6
-    expect(result).to eq(expected_result)
+  context 'test the factorial method in solver class' do
+    it 'factorial returns factorial of the N' do
+      result = solve.factorial(3)
+      expected_result = 6
+      expect(result).to eq(expected_result)
+    end
+
+    it 'returns 1 if the parameter given is 0' do
+      result = solve.factorial(0)
+      expected_result = 1
+      expect(result).to eq(expected_result)
+    end
+
+    it 'should return an error if the given number is negative' do
+      expect { solve.factorial(-1) }.to raise_error('Negative number entered')
+    end
   end
 
-  it 'returns 1 if the parameter given is 0' do
-    result = solve.factorial(0)
-    expected_result = 1
-    expect(result).to eq(expected_result)
-  end
-
-  it 'should return an error if the given number is negative' do
-    expect { solve.factorial(-1) }.to raise_error('Negative number entered')
+  context 'test reverse method in solver class' do
+    it 'returns reversed string' do
+      reversed_string = solve.reverse('artur')
+      expected_str = 'rutra'
+      expect(reversed_string).to eq(expected_str)
+    end
   end
 end
